8132f4b299adf250bf525b238cfa113ef85434da
O.G.A <gbengaadeboye99@gmail.com>
2024-08-01 12:41:42 +0100

    switchhacker

---
+++ assets/js/switch.js
@@ -0,0 +1,73 @@
+let permission = confirm("Are you in for a switch?");
+if(permission==true){
+let userInput= prompt("What is your name?");
+let show;
+let firstletter = userInput[0];
+
+        switch(firstletter){
+            case "a":
+            case "e":
+            case "i":
+            case "o":
+            case "u":
+            
+        show= "A";
+        alert(show);
+        console.log(show);
+    
+        }
+    switch(firstletter){
+            case "b":
+            case "c":
+            case "d":
+            case "f":
+            case "g":
+    
+            show="B";
+            alert(show);
+            console.log(show);
+    }
+        switch(firstletter){
+            case "h":
+            case "j":
+            case "k":
+            case "l":
+            case "m":
+
+            show="C";
+            alert(show);
+            console.log(show);
+    }
+        switch(firstletter){
+            case "n":
+            case "p":
+            case "q":
+            case "r":
+            case "s":
+
+            show="D";
+            alert(show);
+            console.log(show);
+        }
+        switch(firstletter){
+
+            case "t":
+            case "v":
+            case "w":
+
+            show="E";
+            alert(show);
+            console.log(show);
+        }
+        switch(firstletter){
+            case "x":
+            case "y":
+            case "z":
+
+
+            show="F";
+            alert(show);
+            console.log(show);
+        }
+}
+
